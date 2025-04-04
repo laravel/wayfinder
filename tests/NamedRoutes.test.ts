cc4352973fc8849300ae1652fd767cfbe246409a
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-04-04 12:52:06 -0400

    Update NamedRoutes.test.ts

--- tests/NamedRoutes.test.ts
+++ tests/NamedRoutes.test.ts
@@ -1,16 +1,10 @@
 import { expect, it } from "vitest";
-import { home } from "../workbench/resources/js/routes/home";
 import { edit } from "../workbench/resources/js/routes/posts";
 
 it("exports named routes", () => {
     expect(edit.url(1)).toBe("/posts/1/edit");
     expect(edit(1)).toEqual({
         url: "/posts/1/edit",
-        method: "get",
-    });
-    expect(home.url()).toBe("/");
-    expect(home()).toEqual({
-        url: "/",
         method: "get",
     });
 });
