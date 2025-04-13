d47b637965ff0708558b6c7afd59b8d015b01298
Donnie <donniepask@gmail.com>
2025-04-13 14:35:32 +0200

    improved Request class logic

--- src/Request.php
+++ src/Request.php
@@ -11,24 +11,6 @@ class Request
 {
     public Collection $types;
 
-    protected array $optionalRules = [
-        'sometimes', 'present_if', 'present_unless', 'present_with', 'present_with_all', 'required_if', 'required_if_accepted', 'required_if_declined', 'required_unless', 'required_with', 'required_with_all', 'required_without', 'required_without_all', 'missing_if', 'missing_unless', 'missing_with', 'missing_with_all'
-    ];
-
-    protected string $nullable = 'nullable';
-
-    protected Collection $booleans;
-
-    protected Collection $strings;
-
-    protected Collection $numbers;
-
-    protected Collection $arrays;
-
-    protected Collection $any;
-
-    // TODO: should manage min, max? handle Rule class
-
     public function __construct(
         public string $className,
         public array $validations
@@ -48,80 +30,117 @@ class Request
 
             $explodedRules = is_string($validationRules) ? explode('|', $validationRules) : $validationRules;
 
+            $fieldExplicitType = '';
+
             foreach($explodedRules as $rule) {
+                
+                $snakeCaseRule = $rule;
 
-                if (is_string($rule)) {
-
-                    $escapedRule = Str::take($rule, Str::position($rule, ':'));
-                    $escapedRule = $escapedRule === '' ? $rule : $escapedRule;
-
-                    if (in_array($escapedRule, $this->optionalRules)) {
-                        $field .= '?';
-                    }
-
-                    // if (array_key_exists($rule, $this->rules)) {
-                    //     $fieldUnionTypes->push($this->rules[$rule]);
-                    // }
-
-                    // booleans
-                    if($this->booleans()->has($escapedRule)) {
-                        $fieldUnionTypes->push(...$this->booleans()->get($escapedRule));
-                    }
-
-
-                    // strings
-                    if($this->strings()->has($escapedRule)) {
-                        $fieldUnionTypes->push(...$this->strings()->get($escapedRule));
-                    }
-
-                    // numbers
-                    if($this->numbers()->has($escapedRule)) {
-                        $fieldUnionTypes->push(...$this->numbers()->get($escapedRule));
-                    }
-
-                    // arrays
-                    if($this->arrays()->has($escapedRule)) {
-                        $fieldUnionTypes->push(...$this->arrays()->get($escapedRule));
-                    }
-
-                    if($escapedRule === $this->nullable) {
-                        $fieldUnionTypes->push('null');
-                    }
-
-                    // temporary fix for optional fields
-                    if(Str::contains($field, '?') && $fieldUnionTypes->isEmpty()) {
-                        $fieldUnionTypes->push('any');
-                    }
-
-                } else {
-                    // TODO: finish implementation
-                //     // var_dump($rule instanceof Rule);
-
-                //     // if($rule instanceof ValidationRule || $rule instanceof Rule) {
-
-                //     $ruleClass = Str::afterLast(get_class($rule), '\\');
-                //     // var_dump($ruleClass);
-                //     if (in_array($ruleClass, $this->optionalRules)) {
-                //         $field .= '?';
-                //     }
-
-                //     if (array_key_exists(strtolower($ruleClass), $this->rules)) {
-                //         $fieldUnionTypes->push($this->rules[$ruleClass]);
-                //     }
-
-
-                // }
-                    
+                if(! is_string($snakeCaseRule)) {
+                    $snakeCaseRule = Str::snake(Str::afterLast(get_class($snakeCaseRule), '\\'));
                 }
 
+                if(Str::contains($snakeCaseRule, ':')) {
+                    $snakeCaseRule = Str::take($snakeCaseRule, Str::position($snakeCaseRule, ':'));
+                }
+
+                if($this->conditionalRules()->contains($snakeCaseRule)) {
+                    $field .= Str::contains($field, '?') ? '' : '?';
+                }
                 
+                if($this->explicitTypes()->has($snakeCaseRule)){
+                    // $fieldHasExplicitType = true;
+                    $fieldExplicitType = $this->explicitTypes()->get($snakeCaseRule);
+                }
+
+                if($this->booleans()->has($snakeCaseRule)) {
+                    $fieldUnionTypes->push(...$this->booleans()->get($snakeCaseRule));
+                }
+
+                if($this->strings()->has($snakeCaseRule)) {
+                    $fieldUnionTypes->push(...$this->strings()->get($snakeCaseRule));
+                }
+
+                if($this->numbers()->has($snakeCaseRule)) {
+                    $fieldUnionTypes->push(...$this->numbers()->get($snakeCaseRule));
+                }
+
+                if($this->arrays()->has($snakeCaseRule)) {
+                    $fieldUnionTypes->push(...$this->arrays()->get($snakeCaseRule));
+                }
+
+                if($this->files()->has($snakeCaseRule)) {
+                    $fieldUnionTypes->push(...$this->files()->get($snakeCaseRule));
+                }
+
+                if($this->nullables()->has($snakeCaseRule)) {
+                    $fieldUnionTypes->push(...$this->nullables()->get($snakeCaseRule));
+                }
+
             }
 
             if ($fieldUnionTypes->count() > 0) {
-                $rules->push(collect(['field' => $field, 'types' => $fieldUnionTypes->uniqueStrict()->join(' | ')]));
+                $rules->push(collect(['field' => $field, 'types' => $fieldUnionTypes->uniqueStrict()->filter(function(string $type) use ($fieldExplicitType) {
+                    if(Str::length($fieldExplicitType) > 0) {
+
+                        if($fieldExplicitType === 'string') {
+                            return Str::doesntContain($type, ['[', ']', 'File', 'number']);
+                        } else if ($fieldExplicitType === 'number') {
+                            return Str::doesntContain($type, ['[', ']', 'File', 'string']);
+                        }
+                    }
+                    return true;
+                })->join(' | ')]));
             }
         }
         return $rules;
+    }
+
+    protected function explicitTypes() {
+        return collect([
+            'decimal' => 'number',
+            'different' => 'number',
+            'digits' => 'number',
+            'digits_between' => 'number',
+            'gt' => 'number',
+            'gte' => 'number',
+            'integer' => 'number',
+            'lt' => 'number',
+            'lte' => 'number',
+            'max_digits' => 'number',
+            'min_digits' => 'number',
+            'multiple_of' => 'number',
+            'numeric' => 'number',
+            'active_url' => 'string',
+            'alpha' => 'string',
+            'alpha_dash' => 'string',
+            'alpha_numeric' => 'string',
+            'ascii' => 'string',
+            'confirmed' => 'string',
+            'current_password' => 'string',
+            'different' => 'string',
+            'doesnt_start_with' => 'string',
+            'doesnt_end_with' => 'string',
+            'email' => 'string',
+            'ends_with' => 'string',
+            'enum' => 'string',
+            'hex_color' => 'string',
+            'in' => 'string',
+            'ip_address' => 'string',
+            'json' => 'string',
+            'lowercase' => 'string',
+            'mac_address' => 'string',
+            'not_in' => 'string',
+            'regular_expression' => 'string',
+            'not_regular_expression' => 'string',
+            'same' => 'string',
+            'starts_with' => 'string',
+            'string' => 'string',
+            'uppercase' => 'string',
+            'url' => 'string',
+            'ulid' => 'string',
+            'uuid' => 'string',
+        ]);
     }
 
     protected function booleans() {
@@ -155,6 +174,8 @@ class Request
             'json' => ['string'],
             'lowercase' => ['string'],
             'mac_address' => ['string'],
+            'max' => ['string'],
+            'min' => ['string'],
             'not_in' => ['string'],
             'regular_expression' => ['string'],
             'not_regular_expression' => ['string'],
@@ -165,7 +186,7 @@ class Request
             'uppercase' => ['string'],
             'url' => ['string'],
             'ulid' => ['string'],
-            'uuid' => ['string']
+            'uuid' => ['string'],
         ]);
     } 
 
@@ -176,12 +197,14 @@ class Request
             'different' => ['number'],
             'digits' => ['number'],
             'digits_between' => ['number'],
-            'greater_than' => ['number'],
-            'greater_than_or_equal' => ['number'],
+            'gt' => ['number'],
+            'gte' => ['number'],
             'integer' => ['number'],
-            'less_than' => ['number'],
-            'less_than_or_equal' => ['number'],
+            'lt' => ['number'],
+            'lte' => ['number'],
+            'max' => ['number'],
             'max_digits' => ['number'],
+            'min' => ['number'],
             'min_digits' => ['number'],
             'multiple_of' => ['number'],
             'numeric' => ['number'],
@@ -193,21 +216,62 @@ class Request
     protected function arrays() {
         return collect([
             'array' => ['number[]', 'string[]'],
+            'array_rule' => ['number[]', 'string[]'],
             'between' => ['number[]', 'string[]'],
             'contains' => ['number[]', 'string[]'],
             'distinct' => ['number[]', 'string[]'],
             'in_array' => ['number[]', 'string[]'],
+            'max' => ['number[]', 'string[]'],
+            'min' => ['number[]', 'string[]'],
             'list' => ['number[]', 'string[]'],
-            'size' => ['number[]', 'string[]']
+            'size' => ['number[]', 'string[]'],
         ]);
     } 
 
-    protected function any() {
+    /**
+     * The between validation rule is disabled here to avoid conflicts with the same rule for numbers and strings. Same reasoning was made for the size rule.
+     */
+    protected function files() {
         return collect([
-            'file' => ['any'],
-            'fileimage' => ['any'],
-            'image' => ['any']
+            'between' => ['File'],
+            'dimensions' => ['File'],
+            'extensions' => ['File'],
+            'file' => ['File'],
+            'image' => ['File'],
+            'image_file' => ['File'],
+            'max' => ['File'],
+            'mimes' => ['File'],
+            'size' => ['File'],
         ]);
-    } 
+    }
+
+    protected function nullables() {
+        return collect([
+            'nullable' => ['null']
+        ]);
+    }
+
+    protected function conditionalRules() {
+        return collect([
+            'sometimes',
+            'present_if',
+            'present_unless',
+            'present_with',
+            'present_with_all',
+            'required_if',
+            'required_if_accepted',
+            'required_if_declined',
+            'required_unless',
+            'required_with',
+            'required_with_all',
+            'required_without',
+            'required_without_all',
+            'missing_if',
+            'missing_unless',
+            'missing_with',
+            'missing_with_all',
+            'conditional_rules'
+        ]);
+    }
 
 }
