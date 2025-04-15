721b4b161ba9ffed1d28f69479cd94874560ea3a
Tim MacDonald <hello@timacdonald.me>
2025-04-16 09:55:43 +1000

    Add support for generating route while routes are cached

--- build.ts
+++ build.ts
@@ -1,17 +1,25 @@
 import { execSync } from "node:child_process";
 import path from "node:path";
 
-export default function setup() {
-    try {
-        const testbenchDir = path.join(__dirname, "vendor", "bin", "testbench");
+const testbenchDir = path.join(__dirname, "vendor", "bin", "testbench");
+const artisan = (command: string): void => console.error(execSync(`${testbenchDir} ${command}`).toString('utf8'))
 
-        execSync(
-            `${testbenchDir} wayfinder:generate --path=workbench/resources/js --with-form`,
-        );
+export function setup(): void {
+    try {
+        if (process.env.WAYFINDER_CACHE_ROUTES) {
+            artisan('route:cache')
+        } else {
+            artisan('route:clear')
+        }
+
+        artisan('wayfinder:generate --path=workbench/resources/js --with-form')
     } catch (error) {
-        console.error(
-            `Wayfinder build error.\n----------${error.output}\n----------`,
-        );
+        console.error(`Wayfinder build error\n----------${error.output}\n----------`);
+
         process.exit(1);
     }
+}
+
+export function teardown(): void {
+    artisan('route:clear')
 }
