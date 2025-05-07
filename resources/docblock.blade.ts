d7c5bad82e4942f56c895c7dd7e246dd85c93001
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-05-07 10:08:43 -0400

    moved logic from the view to Route

--- resources/docblock.blade.ts
+++ resources/docblock.blade.ts
@@ -1,5 +1,5 @@
 /**{!! when(!str_contains($controller, '\\Closure'), PHP_EOL . " * @see {$controller}::" . ($isInvokable ? '__invoke' : $docblock_method ?? $method)) !!}
- * @see {!! str_replace(DIRECTORY_SEPARATOR, '/', $path) !!}:{!! $line !!}
+ * @see {!! $path !!}:{!! $line !!}
 @foreach ($parameters as $parameter)
 @if ($parameter->default !== null)
  * @param {!! $parameter->name !!} - Default: @js($parameter->default)
