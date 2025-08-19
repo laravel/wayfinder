d09a3582ecf7b1d5d45956ad833b893477dca0a2
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-08-19 14:25:02 -0400

    Update DomainController.test.ts

--- tests/DomainController.test.ts
+++ tests/DomainController.test.ts
@@ -1,13 +1,20 @@
 import { expect, test } from "vitest";
-import { dynamicDomain, fixedDomain } from "../workbench/resources/js/actions/App/Http/Controllers/DomainController";
+import {
+    defaultParametersDomain,
+    fixedDomain,
+} from "../workbench/resources/js/actions/App/Http/Controllers/DomainController";
 
-test('can generate fixed domain urls', () => {
-    expect(fixedDomain.url({ param: 'foo' })).toBe('//example.test/fixed-domain/foo')
-})
+test("can generate fixed domain urls", () => {
+    expect(fixedDomain.url({ param: "foo" })).toBe(
+        "//example.test/fixed-domain/foo",
+    );
+});
 
-test('can generate dynamic domain urls', () => {
-    expect(dynamicDomain.url({
-        domain: 'tim.macdonald',
-        param: 'foo',
-    })).toBe('//tim.macdonald.au/dynamic-domain/foo')
-})
+test("can generate dynamic domain urls", () => {
+    expect(
+        defaultParametersDomain.url({
+            defaultDomain: "tim.macdonald",
+            param: "foo",
+        }),
+    ).toBe("//tim.macdonald.au/default-parameters-domain/foo");
+});
