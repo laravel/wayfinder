931b9edc3974b2f77ddda4f6ea6bb54f5dd0ee9e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-08-19 15:02:59 -0400

    simplified eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,13 @@
-import { defineConfig } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import { fixupPluginRules } from "@eslint/compat";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
-
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        import: fixupPluginRules(_import),
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.sourceType,
+export default tseslint.config(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["workbench/resources/js/**/*.{ts,tsx}"],
+        rules: {
+            "no-duplicate-imports": "error",
         },
-
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
     },
-
-    rules: {
-        "import/no-duplicates": "error",
-    },
-}]);
+);
