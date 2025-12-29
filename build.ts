3639e049f06ce92cb17bc1f28a4e5afcca8b82a1
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-12-29 10:03:07 -0500

    wip

--- build.ts
+++ build.ts
@@ -1,27 +1,26 @@
 import { execSync } from "node:child_process";
-import { copyFileSync } from "node:fs";
 import path from "node:path";
 
 const testbenchDir = path.join(__dirname, "vendor", "bin", "testbench");
 const baseDir = path.join(__dirname, "workbench");
 const appDir = path.join(baseDir, "app");
 
-const artisan = (command: string): void =>
-    console.error(execSync(`${testbenchDir} ${command}`).toString("utf8"));
+const artisan = (command: string, env: Record<string, string> = {}): void =>
+    console.error(
+        execSync(`${testbenchDir} ${command}`, {
+            env: { ...process.env, ...env },
+        }).toString("utf8"),
+    );
 
 export function setup(): void {
     try {
-        copyFileSync(
-            path.join(__dirname, "tests", "fixtures", "env.stub"),
-            path.join(baseDir, ".env"),
-        );
-
         process.env.WAYFINDER_CACHE_ROUTES
             ? artisan("route:cache")
             : artisan("route:clear");
 
         artisan(
             `wayfinder:generate --path=workbench/resources/js/wayfinder --app-path=${appDir} --base-path=${baseDir}`,
+            { VITE_APP_NAME: "Workbench" },
         );
     } catch (error) {
         console.error(
