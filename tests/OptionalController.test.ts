9d3d3929f8760ca05883c297d27269dc3174ab92
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-04-07 09:05:54 -0700

    Update OptionalController.test.ts

--- tests/OptionalController.test.ts
+++ tests/OptionalController.test.ts
@@ -29,6 +29,7 @@ describe("manyOptional", async () => {
 
     test("url supports falsy optional values", () => {
         expect(manyOptional.url({ one: 0, two: 2 })).toBe("/many-optional/0/2");
+        expect(manyOptional.url({ one: 0 })).toBe("/many-optional/0");
     });
 
     it("throws an error when passing optional parameters with missing optional parameters before", () => {
