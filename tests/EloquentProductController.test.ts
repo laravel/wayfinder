8fd8c955177a8f3aadaae77fe28ca4669127ca80
Levi Klingler <levikl@geeked.gg>
2026-05-13 18:05:11 -0400

    backport `expectTypeOf` testing strategy to eloquent model tests

--- tests/EloquentProductController.test.ts
+++ tests/EloquentProductController.test.ts
@@ -6,14 +6,14 @@ type NotAny<T> = 0 extends (1 & T) ? never : T;
 
 describe("EloquentProductController", () => {
     test("Index narrows products to App.Models.Product[]", () => {
-        expectTypeOf<NotAny<Inertia.Pages.EloquentProducts.Index["products"]>>().toEqualTypeOf<
-            App.Models.Product[]
-        >();
+        type Subject = NotAny<Inertia.Pages.EloquentProducts.Index["products"]>;
+
+        expectTypeOf<Subject>().toEqualTypeOf<App.Models.Product[]>();
     });
 
     test("Show narrows product to App.Models.Product", () => {
-        expectTypeOf<NotAny<Inertia.Pages.EloquentProducts.Show["product"]>>().toEqualTypeOf<
-            App.Models.Product
-        >();
+        type Subject = NotAny<Inertia.Pages.EloquentProducts.Show["product"]>;
+
+        expectTypeOf<Subject>().toEqualTypeOf<App.Models.Product>();
     });
 });
