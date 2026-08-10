aabff9b23eaa905cd31579a51448b865867e2983
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-08-10 14:13:46 -0700

    remove duplicate tests

--- tests/Unit/Registry/TypeScriptConverterTest.php
+++ tests/Unit/Registry/TypeScriptConverterTest.php
@@ -52,22 +52,4 @@ class TypeScriptConverterTest extends TestCase
 
         $this->assertSame('unknown[]', $result);
     }
-
-    public function test_json_cast_produces_record_type(): void
-    {
-        $arrayShape = Types\Type::arrayShape(Types\Type::mixed(), Types\Type::mixed());
-
-        $result = $this->converter->convert($arrayShape);
-
-        $this->assertSame('Record<string, unknown>', $result);
-    }
-
-    public function test_array_cast_produces_array_type(): void
-    {
-        $arrayType = Types\Type::array([]);
-
-        $result = $this->converter->convert($arrayType);
-
-        $this->assertSame('unknown[]', $result);
-    }
 }
