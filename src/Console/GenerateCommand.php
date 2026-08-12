94a11945bd85ecb6aa54a20fe36e1c67173376f1
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-08-12 16:24:30 -0700

    Reuse file modification times during generation (#300)
    * use new freeze file times in surveyor
    
    * Update GenerateCommand.php

--- src/Console/GenerateCommand.php
+++ src/Console/GenerateCommand.php
@@ -59,6 +59,7 @@ class GenerateCommand extends Command
         Enums $enumConverter,
         Routes $routesConverter,
     ) {
+        AnalyzedCache::freezeFileTimes();
         AnalyzedCache::setCacheDirectory($this->config->get('wayfinder.cache.directory'));
 
         if ($this->option('fresh') || ! $this->config->get('wayfinder.cache.enabled')) {
